c938b5f43b94d87f24705c0031d0c70da7ccd6b1
Ali Hallaji <ali.hallaji1@gmail.com>
2017-02-16 23:54:02 +0330

    Update __init__.py

--- core/db/__init__.py
+++ core/db/__init__.py
@@ -1,5 +1,3 @@
-from config.settings import EXIST_DB
-from config.settings import DB_NAME
 from mongo_drivers import MongoConnection
 
 
@@ -7,5 +5,5 @@ def MongoCursor(db):
     return MongoConnection().getCursor(db)
 
 
-if EXIST_DB:
-    cursor = MongoCursor(DB_NAME)
+if MongoConnection.exist:
+    cursor = MongoCursor(MongoConnection.db_name)
